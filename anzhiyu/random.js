5e0b674b74de13ace0c4dda33d868763ab2d569a
DaiShiBo369 <17857688918@163.com>
2026-01-21 00:20:36 +0800

    Site updated: 2026-01-21 00:20:36

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2026/01/17/2026-01-16-Disboundia-开发日志/","2026/01/17/2026-01-17-Disboundia-开发日志/","2026/01/18/2026-01-18-Disboundia-开发日志/","2025/11/07/hello-world/"];function toRandomPost(){
+var posts=["2026/01/17/2026-01-17-Disboundia-开发日志/","2026/01/17/2026-01-16-Disboundia-开发日志/","2026/01/18/2026-01-18-Disboundia-开发日志/","2025/11/07/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
