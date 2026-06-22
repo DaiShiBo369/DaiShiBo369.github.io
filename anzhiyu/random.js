8e9b8234eb5e5c8b5990a76ca2b1327b2010c97f
戴士博 <17857688918@163.com>
2026-06-22 23:37:28 +0800

    Site updated: 2026-06-22 23:37:27

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2026/01/17/2026-01-16-Disboundia-开发日志/","2026/01/17/2026-01-17-Disboundia-开发日志/","2026/04/29/2026-04-29-music-gallery-dev-log/","2026/01/18/2026-01-18-Disboundia-开发日志/","2026/06/20/Linux虚拟机配置/","2025/11/07/hello-world/","2026/01/24/深广小记/"];function toRandomPost(){
+var posts=["2026/01/17/2026-01-16-Disboundia-开发日志/","2026/01/18/2026-01-18-Disboundia-开发日志/","2026/01/17/2026-01-17-Disboundia-开发日志/","2026/04/29/2026-04-29-music-gallery-dev-log/","2025/11/07/hello-world/","2026/01/24/深广小记/","2026/06/20/Linux虚拟机配置/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
